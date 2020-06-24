6781830a4fb80f70943635d2ad9c84cbda8562f1
Chelsea Wetzel <chelswetz@gmail.com>
2020-06-24 17:50:20 -0400

    add to Grid

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -4,23 +4,44 @@ import "./index.css";
 
 class Grid extends React.Component {
   render() {
-    
+    const width = this.props.cols * 14;
+    var rowsArr = []
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      
+    }
+    return (
+      <div className="grid" style={{width: width}}>
+        {{rowsArr}}
+      </div>
+    )
   }
 }
 
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
   }
+
   render() {
     return (
       <div>
-        <h1>The Game of Life - By Chelsea Wetzel</h1>
-        <Grid/>
+        <h1>The Game of Life</h1>
+        <Grid
+          gridFull={this.state.gridFull} // from our state
+          rows={this.rows} // props from Grid
+          cols={this.cols}/>  
         <h2>Generation: {this.state.generation}</h2>
+        <p>Created By Chelsea Wetzel</p>
       </div>
     );
   }
